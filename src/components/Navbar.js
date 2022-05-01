61b3b91e16e261ea23efe0512243bdcefdaa49e0
Quinn Molina <quinnmolina@Quinns-MacBook-Pro.local>
2022-05-01 16:59:21 -0700

    logout hook/button added to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout';
 
 // styles
 import styles from './Navbar.module.css';
 
 function Navbar() {
+  const { logout } = useLogout()
+
   return (
     <nav className={styles.navbar}>
         <ul>
@@ -11,6 +14,10 @@ function Navbar() {
 
             <li> <Link to ="/login">Login</Link></li>
             <li> <Link to ="/signup">Signup</Link></li>
+
+            <li>
+              <button className="btn" onClick={logout}>Log Out</button>
+            </li>
         </ul>
     </nav>
   )
