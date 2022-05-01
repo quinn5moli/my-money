d5cbcb662a542cc30a4508bbd9836a1be0b482d5
Quinn Molina <quinnmolina@Quinns-MacBook-Pro.local>
2022-05-01 12:47:20 -0700

    firebase config setup

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCPmXN5zDU1a6uvmqxT6CjCcU3y9Q2FsTs",
+    authDomain: "mymoney-c0750.firebaseapp.com",
+    projectId: "mymoney-c0750",
+    storageBucket: "mymoney-c0750.appspot.com",
+    messagingSenderId: "892487340490",
+    appId: "1:892487340490:web:e1cccceba14b420763ff33"
+  };
+
+  // init firebase
+
+  firebase.initializeApp(firebaseConfig)
+
+  // init service
+  const projectFirestore = firebase.firestore()
+
+  export { projectFirestore }
