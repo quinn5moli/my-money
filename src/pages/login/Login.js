01e5e2083e8e5ec5ae722e0f9fc0a8eb18c480d4
Quinn Molina <quinnmolina@Quinns-MacBook-Pro.local>
2022-05-01 11:46:14 -0700

    login form created

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,38 @@
+import { useState } from 'react'
+
+//styles
 import styles from './Login.module.css';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
+
   return (
-    <div>Login</div>
+    <form onSubmit={handleSubmit} className={styles['login-form']}>
+      <h2>Login</h2>
+      <label>
+        <span> email:</span>
+        <input 
+          type="email" 
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span> password:</span>
+        <input 
+          type="password"
+           onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Login</button>
+    </form>
   )
 }
 
