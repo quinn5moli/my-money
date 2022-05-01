cb720ad9a29f441774f16f6994591fc954f1d64e
Quinn Molina <quinnmolina@Quinns-MacBook-Pro.local>
2022-05-01 16:39:04 -0700

    logout hook created and added to AuthContext

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import { projectAuth } from "../firebase/config"
+import { useAuthContext } from "./useAuthContext"
+
+const useLogout = () => {
+    const [error, setError] = useState(null)
+    const [isPending, setIsPending] = useState(false)
+    const { dispatch } = useAuthContext()
+
+    const logout = async () => {
+        setError(null)
+        setIsPending(true)
+
+        // sign the user out
+        try {
+            await projectAuth.signOut()
+
+            // dispatch logout action
+            dispatch({ type: "LOGOUT" })
+
+            setIsPending(false)
+            setError(null)
+        }
+        catch (err) {
+            console.log(err.message)
+            setError(err.message)
+            setIsPending(false)
+        }
+    }
+
+    return { error, isPending, logout }
+}
