7766a466005a69fef31b9f5172af51b3710414ad
Quinn Molina <quinnmolina@Quinns-MacBook-Pro.local>
2022-05-01 17:35:01 -0700

    useLogin hook created

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react"
+import { projectAuth } from "../firebase/config"
+import { useAuthContext } from "./useAuthContext"
+
+export const useLogin = () => {
+    const [isCancelled, setIsCancelled] = useState(false)
+    const [error, setError] = useState(null)
+    const [isPending, setIsPending] = useState(false)
+    const { dispatch } = useAuthContext()
+
+    const login = async () => {
+        setError(null)
+        setIsPending(true)
+
+        // sign the user out
+        try {
+            await projectAuth.signInWithEmailAndPassword(email, password)
+
+            // dispatch logout action
+            dispatch({ type: "LOGIN", payload: res.user })
+
+            // update state
+            if(!isCancelled) {
+                setIsPending(false)
+                setError(null)
+            }
+        }
+        catch (err) {
+            if(!isCancelled) {
+                console.log(err.message)
+                setError(err.message)
+                setIsPending(false)
+            }
+        }
+    }
+
+    useEffect(() => {
+        return () => {
+            setIsCancelled(true)
+        }
+    }, [])
+
+    return { error, isPending, login }
+}
