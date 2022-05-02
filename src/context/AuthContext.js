552d6fe1bd57b4950bca1b30fd98f8d3d1554af1
Quinn Molina <quinnmolina@Quinns-MacBook-Pro.local>
2022-05-01 19:02:38 -0700

    AUTH_IS_READY added to authContext

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,13 +1,16 @@
-import { createContext, useReducer } from "react"
+import { createContext, useEffect, useReducer } from "react"
+import { projectAuth } from "../firebase/config"
 
 export const AuthContext = createContext()
 
 export const authReducer = (state, action) => {
     switch (action.type) {
         case "LOGIN":
-            return { ...state, user: action.payload }
+          return { ...state, user: action.payload }
         case "LOGOUT":
-            return { ...state, user: null }
+          return { ...state, user: null }
+        case "AUTH_IS_READY":
+          return { ...state, user: action.payload, authIsReady: true }
       default:
         return state
     }
@@ -15,8 +18,17 @@ export const authReducer = (state, action) => {
 
 export const AuthContextProvider = ({children}) => {
     const [state, dispatch] = useReducer(authReducer, {
-        user: null,
+      user: null,
+      authIsReady: false
     })
+
+    useEffect(() => {
+        const unsub = projectAuth.onAuthStateChanged((user) => {
+          dispatch({ type: "AUTH_IS_READY", payload: user })
+          unsub()
+        })
+    },[])
+
     console.log('AuthContext state: ', state)
 
     return (
