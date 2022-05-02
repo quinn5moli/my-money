7e15454c5a66d0818c3315383fb908e7bfa52891
Quinn Molina <quinnmolina@Quinns-MacBook-Pro.local>
2022-05-01 18:52:29 -0700

    conditionals added to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,34 @@
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 // styles
 import styles from './Navbar.module.css';
 
 function Navbar() {
   const { logout } = useLogout()
+  const { user } = useAuthContext()
 
   return (
     <nav className={styles.navbar}>
         <ul>
             <li className={styles.title}> myMoney </li>
 
-            <li> <Link to ="/login">Login</Link></li>
-            <li> <Link to ="/signup">Signup</Link></li>
+          {!user && (
+            <>
+              <li> <Link to ="/login">Login</Link></li>
+              <li> <Link to ="/signup">Signup</Link></li>
+            </>
+          )}
 
-            <li>
-              <button className="btn" onClick={logout}>Log Out</button>
-            </li>
+          {user && (
+            <>
+              <li>hello, {user.displayName}</li>
+              <li>
+                <button className="btn" onClick={logout}>Log Out</button>
+              </li>
+            </>
+          )}
         </ul>
     </nav>
   )
