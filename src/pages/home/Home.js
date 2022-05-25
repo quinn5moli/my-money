787cbde3ae608b063304f1ac2258c10eeb3cd1af
Quinn Molina <quinnmolina@Quinns-MacBook-Pro.local>
2022-05-24 20:24:00 -0700

    integrated useFirestore hook with TransactionForm

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,21 @@
+import { useAuthContext } from '../../hooks/useAuthContext'
+
+// styles
 import styles from './Home.module.css';
 
+// components
 import TransactionForm from './TransactionForm'
 
 function Home() {
+  const { user } = useAuthContext()
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         transaction list
       </div>
       <div className={styles.sidebar}>
-        <TransactionForm />
+        <TransactionForm uid={user.uid} />
       </div>
     </div>
   )
--- src/pages/home/TransactionForm.js
+++ src/pages/home/TransactionForm.js
@@ -1,19 +1,33 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import { useFirestore } from '../../hooks/useFirestore'
 
-export default function TransactionForm() {
+export default function TransactionForm({ uid }) {
     const [name, setName] = useState('')
     const [amount, setAmount] = useState('')
+    const { addDocument, response } = useFirestore('transactions')
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(name, amount)
+        addDocument({
+            uid, 
+            name, 
+            amount
+        })
     }
+
+    // reset the form fields
+    useEffect(() => {
+        if (response.success) {
+            setName('')
+            setAmount('')
+        }
+    }, [response.success])
 
 
   return (
     <>
         <h3>Add a Transaction</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 <span> Transaction name:</span>
                 <input 
@@ -26,7 +40,7 @@ export default function TransactionForm() {
             <label>
                 <span> Amount ($)</span>
                 <input
-                    type="text"
+                    type="number"
                     required
                     onChange={(e) => setAmount(e.target.value)}
                     value={amount}
